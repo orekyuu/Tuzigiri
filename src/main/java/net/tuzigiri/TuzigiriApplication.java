7173e9911133ae6955a4303169f66fa0b53757ca
orekyuu <orekyuu@yahoo.co.jp>
2018-09-13 23:57:09 +0900

    add spring boot

---
+++ src/main/java/net/tuzigiri/TuzigiriApplication.java
@@ -0,0 +1,12 @@
+package net.tuzigiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuzigiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuzigiriApplication.class, args);
+	}
+}
